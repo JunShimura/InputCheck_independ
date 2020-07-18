b5d465472469a2a57b134f928ff6271c7eea22f4
Jun Shimura <junshimura@gmail.com>
2020-07-18 10:09:21 +0900

    プロジェクト ファイルを追加します。

---
+++ InputCheck.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Commontools
+{
+    public class InputCheck
+    {
+        public static float GetNumber(string meaning, float min, float max)
+        {
+            float value;
+            // 入力処理
+            while (true)
+            {
+                Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+                value = int.Parse(Console.ReadLine());
+                if (value >= min && value < max)
+                { //値は適正
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("入力エラー");
+                }
+            }
+            return value;
+        }
+
+        public static int GetNumber(string meaning, int min, int max)
+        {
+            int value;
+            // 入力処理
+            while (true)
+            {
+                Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+                value = int.Parse(Console.ReadLine());
+                if (value >= min && value < max)
+                { //値は適正
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("入力エラー");
+                }
+            }
+            return value;
+        }
+
+        /*
+        public static T GetNumber<T>(string meaning, T min, T max) where T:IComparable
+        {
+            T value ;
+            // 入力処理
+            while (true)
+            {
+                Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+                string s = Console.ReadLine();
+                if (value is int){
+
+                }
+                value =Converter<string,T>(Console.ReadLine());
+                if (value.CompareTo(min)>0 && value.CompareTo(max)<0 )
+                { //値は適正
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("入力エラー");
+                }
+            }
+            return value;
+        }
+        */
+    }
+}
+
---
+++ InputCheckTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Commontools;
+
+class InputCheckTest
+{
+    static int rMin = 1;   //半径の最小値
+    static int rMax = 100; //半径の最大値
+    static int hMin = 1;   //高さの最小値
+    static int hMax = 300; //高さの最大値
+    static void Main(string[] args)
+    {
+        float r = InputCheck.GetNumber("半径", rMin, rMax);
+        float h = InputCheck.GetNumber("高さ", hMin, hMax);
+        float s = 0; //底面積
+        float v = 0; //体積
+        s = GetCircleSurface(r);
+        v = GetCylinderVolume(r, h);
+        Console.WriteLine("底面積は{0},体積は{1}", s, v);
+    }
+
+    static float GetCircleSurface(float radius)
+    {  //半径から円の面積を求める
+        float surface = 0;
+        //　ここに処理を追加
+        surface = (float)Math.PI * radius * radius;
+        return surface;
+    }
+    static float GetCylinderVolume(float radius, float height)
+    {  //半径と高さから円柱の体積を求める
+        float volume = GetCircleSurface(radius) * height;
+        return volume;
+    }
+}
+

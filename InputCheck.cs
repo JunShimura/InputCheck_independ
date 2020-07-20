e5324eee8cec9ca99df119092e9d6b708934e525
Jun Shimura <junshimura@gmail.com>
2020-07-20 15:54:53 +0900

    TyrParse

--- InputCheck.cs
+++ InputCheck.cs
@@ -1,48 +1,62 @@
 ﻿using System;
 
-namespace Commontools
+class InputCheck
 {
-    public class InputCheck
-    {
-        public static float GetNumber(string meaning, float min, float max)
+    static public int GetNumber(string meaning = null, int min = 0, int max = int.MaxValue)
+    {   //int型で動く
+        int value;
+        // 入力処理
+        while (true)
         {
-            float value;
-            // 入力処理
-            while (true)
-            {
-                Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
-                value = int.Parse(Console.ReadLine());
-                if (value >= min && value < max)
-                { //値は適正
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("入力エラー");
-                }
+            Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+            bool success = int.TryParse(Console.ReadLine(), out value);
+            if (success && value >= min && value < max)
+            { //値は適正
+                break;
             }
-            return value;
+            else
+            {
+                Console.WriteLine("入力エラー");
+            }
         }
-
-        public static int GetNumber(string meaning, int min, int max)
+        return value;
+    }
+    static public float GetNumber(string meaning = null, float min = 0, float max = float.MaxValue)
+    {  //float型で動く
+        float value;
+        // 入力処理
+        while (true)
         {
-            int value;
-            // 入力処理
-            while (true)
-            {
-                Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
-                value = int.Parse(Console.ReadLine());
-                if (value >= min && value < max)
-                { //値は適正
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("入力エラー");
-                }
+            Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+            bool success = float.TryParse(Console.ReadLine(), out value);
+            if (success && value >= min && value < max)
+            { //値は適正
+                break;
             }
-            return value;
+            else
+            {
+                Console.WriteLine("入力エラー");
+            }
         }
+        return value;
+    }
+    static public double GetNumber(string meaning = null, double min = 0, double max = double.MaxValue)
+    {  //float型で動く
+        double value;
+        // 入力処理
+        while (true)
+        {
+            Console.WriteLine("{0}は幾つですか？\n範囲は{1}から{2}", meaning, min, max);
+            bool success = double.TryParse(Console.ReadLine(), out value);
+            if (success && value >= min && value < max)
+            { //値は適正
+                break;
+            }
+            else
+            {
+                Console.WriteLine("入力エラー");
+            }
+        }
+        return value;
     }
 }
-
